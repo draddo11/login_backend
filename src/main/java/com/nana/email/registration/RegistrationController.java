9ac8169311f8e685596d672d4527748cd2d880df
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 18:42:07 +0000

    added the registrration controller and service

--- src/main/java/com/nana/email/registration/RegistrationController.java
+++ src/main/java/com/nana/email/registration/RegistrationController.java
@@ -1,5 +1,17 @@
 package com.nana.email.registration;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/registration")
+@AllArgsConstructor
 public class RegistrationController {
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
 
 }
---
+++ src/main/java/com/nana/email/registration/RegistrationService.java
@@ -0,0 +1,4 @@
+package com.nana.email.registration;
+
+public class RegistrationService {
+}
