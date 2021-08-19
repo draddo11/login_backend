815d81659e656bed0d9798884441a145c1c219df
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 18:28:17 +0000

    resolved the findByEmail

--- src/main/java/com/nana/email/appuser/AppUserService.java
+++ src/main/java/com/nana/email/appuser/AppUserService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class AppUserService<AppUserRepository> implements UserDetailsService {
+public class AppUserService  implements UserDetailsService {
 
     private final static String USER_NOT_FOUND_MSG =
             "user with email %s not found";
---
+++ src/main/java/com/nana/email/registration/RegistrationController.java
@@ -0,0 +1,5 @@
+package com.nana.email.registration;
+
+public class RegistrationController {
+
+}
