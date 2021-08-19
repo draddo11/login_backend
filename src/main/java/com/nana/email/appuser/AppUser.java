0381b9791e4d4ac1bbcfe907080db30dd4ee1e16
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 15:37:32 +0000

    adding the rols and conditions

--- src/main/java/com/nana/email/appuser/AppUser.java
+++ src/main/java/com/nana/email/appuser/AppUser.java
@@ -1,43 +1,56 @@
 package com.nana.email.appuser;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class AppUser implements UserDetails{
+
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private  AppUserRole appUserRole;
+    private  Boolean locked ;
+    private Boolean enabled ;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+       SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return  password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
