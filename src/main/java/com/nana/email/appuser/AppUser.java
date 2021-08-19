6a295a5d9bfc53663937d2e87d38154841871e6a
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 16:25:44 +0000

    added the studnt repo with transactional and repo  nnotations

--- src/main/java/com/nana/email/appuser/AppUser.java
+++ src/main/java/com/nana/email/appuser/AppUser.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -19,11 +19,22 @@ import java.util.Collections;
 @Entity
 public class AppUser implements UserDetails{
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String username;
     private String password;
     private String email;
+    @Enumerated(EnumType.STRING)
     private  AppUserRole appUserRole;
     private  Boolean locked ;
     private Boolean enabled ;
