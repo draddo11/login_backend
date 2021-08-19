4676db18aeff9f4e9a0fbd14685e68d4b9f6d6b0
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 19:08:18 +0000

    added the contoller for registrarion and request

--- src/main/java/com/nana/email/registration/RegistrationController.java
+++ src/main/java/com/nana/email/registration/RegistrationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
+    private RegistrationService registrationService;
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
--- src/main/java/com/nana/email/registration/RegistrationService.java
+++ src/main/java/com/nana/email/registration/RegistrationService.java
@@ -1,4 +1,7 @@
 package com.nana.email.registration;
 
 public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
 }
