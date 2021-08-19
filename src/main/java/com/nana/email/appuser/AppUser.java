068142b1aaec440184ae2c41e4adfd47352962b3
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 16:02:29 +0000

    added the entity annotation

--- src/main/java/com/nana/email/appuser/AppUser.java
+++ src/main/java/com/nana/email/appuser/AppUser.java
@@ -1,12 +1,22 @@
 package com.nana.email.appuser;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
 import java.util.Collection;
 import java.util.Collections;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 public class AppUser implements UserDetails{
 
     private Long id;
@@ -17,6 +27,24 @@ public class AppUser implements UserDetails{
     private  AppUserRole appUserRole;
     private  Boolean locked ;
     private Boolean enabled ;
+
+//    generated constructor without id
+    public AppUser(String name,
+                   String username,
+                   String password,
+                   String email,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
        SimpleGrantedAuthority authority =
