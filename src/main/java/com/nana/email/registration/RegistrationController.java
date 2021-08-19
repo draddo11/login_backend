1a681814ea858adb3f6e697f3a7f96cc5c14a95c
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 23:17:38 +0000

    added postmapping to the registration cointroller

--- src/main/java/com/nana/email/registration/RegistrationController.java
+++ src/main/java/com/nana/email/registration/RegistrationController.java
@@ -2,6 +2,7 @@ package com.nana.email.registration;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
+
     private RegistrationService registrationService;
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
