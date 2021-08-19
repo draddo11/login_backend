1d453a11a9eddfc62f9e3b7e838ee93b38f5aa9d
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 22:28:30 +0000

    db and table up and running

--- src/main/java/com/nana/email/registration/RegistrationService.java
+++ src/main/java/com/nana/email/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.nana.email.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "Works";
