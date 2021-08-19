96ae5cf79ca985db0c7321b71d8c71f71de316cb
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-19 21:37:56 +0000

    added the encrtption to the websecurity

--- src/main/java/com/nana/email/security/PasswordEncoder.java
+++ src/main/java/com/nana/email/security/PasswordEncoder.java
@@ -1,4 +1,15 @@
 package com.nana.email.security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/nana/email/security/config/WebSecurityConfig.java
+++ src/main/java/com/nana/email/security/config/WebSecurityConfig.java
@@ -1,26 +1,34 @@
 package com.nana.email.security.config;
 
 
+import com.nana.email.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
       http
               .csrf().disable()
               .authorizeRequests()
-              .antiMatchers("/api/v*/registration/**")
+              .antMatchers("/api/v*/registration/**")
               .permitAll()
+              .anyRequest()
               .authenticated().and()
               .formLogin();
     }
@@ -33,8 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public DaoAuthenticationProvider daoAuthenticationProvider(){
         DaoAuthenticationProvider provider =
                 new DaoAuthenticationProvider();
-        provider.setPasswordEncoder();
-        provider.setUserDetailsService();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
         return provider
     }
 }
