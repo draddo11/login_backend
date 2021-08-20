73831e7fe0f2dfdf85cf2c4929eccd0333538af0
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-20 20:51:07 +0000

    added the Bcryt encoder to the app user service

--- src/main/java/com/nana/email/appuser/AppUserService.java
+++ src/main/java/com/nana/email/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class AppUserService  implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG =
             "user with email %s not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email)
